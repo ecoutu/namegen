22edbc5d0379688d3c62dc0fec5d61bb0f887748
Eric Coutu <eric.coutu@gmail.com>
2013-11-07 21:31:18 -0500

    listen on port 80

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 
 var app = express();
 
-app.listen(3000);
+app.listen(80);
 
 var wordList = fs.readFileSync("words").toString().split("\n");
 var wordListLength = wordList.length;
