19bb1c00ac70e527f8af06ca82916e4eb4b4ea55
Eric Coutu <eric.coutu@gmail.com>
2014-01-01 23:27:12 -0500

    Added logging, plus some refactoring

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
+var _ = require("underscore");
 var express = require("express");
 var fs = require("fs");
 
+var wordListPath = "words";
+var wordList = fs.readFileSync(wordListPath).toString().split("\n");
+var wordListLength = wordList.length;
+console.log("Read " + wordListLength + " words from file " + wordListPath);
+
 var app = express();
 
-app.listen(80);
-
-var wordList = fs.readFileSync("words").toString().split("\n");
-var wordListLength = wordList.length;
+app.use(express.logger("dev"));
+app.listen(8080);
 
 app.get("/bandname", function(request, response) {
-    var num = 1;
-    var words = 2;
+    var num = parseInt(request["query"]["num"]) || 1;
+    var words = parseInt(request["query"]["words"]) || 2;
     var bandNames = "";
 
-    if ("words" in request["query"]) {
-        words = parseInt(request["query"]["words"]);
-    }
-
-    if ("num" in request["query"]) {
-        num = parseInt(request["query"]["num"]);
-    }
-
     for (var i = 0; i < num; i++) {
+        var name = "";
         for (var j = 0; j < words; j++) {
-            bandNames += " " + wordList[Math.floor(Math.random() * wordListLength)];
+            name += " " + wordList[Math.floor(Math.random() * wordListLength)];
         }
-        bandNames += "<br />";
+        bandNames += name.trim() + "<br />";
     }
 
     html = "<html><body>" + bandNames + "</body></html>"
 
-    response.send(bandNames);
+    response.send(html);
 });
