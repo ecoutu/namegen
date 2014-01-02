556d64a11df44fedee0ae07df97c60c6e17e9797
Eric Coutu <eric.coutu@gmail.com>
2014-01-02 01:24:31 -0500

    Refactoring + more logging

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
-var argv = require("optimist")
-    , config = require("./config.json")[process.env.ENV]
+var config = require("./config.json")[process.env.ENV]
     , express = require("express")
     , fs = require("fs")
+    , optimist = require("optimist")
     ;
 
-argv =  argv.usage("Random name generator")
+var defaultPort = process.env.PORT || config.port || 7677;
+
+// Command line arguments
+argv =  optimist.usage("Random name generator")
     .default("c", config.defaultCount)
         .alias("c", "count")
         .describe("c", "Number of words / name")
     .default("f", "words")
-        .alias("f", "wordfile")
+        .alias("f", "file")
         .describe("f", "Path to the wordlist file")
     .default("n", config.defaultNum)
         .alias("n", "numnames")
         .describe("n", "Default number of names to generate")
-    .default("p", process.env.PORT || config.port || 7677)
+    .default("p", defaultPort)
         .alias("p", "port")
         .describe("p", "Port to listen on")
-    .argv;
+    .default("r", config.defaultRoute)
+        .alias("r", "route")
+        .describe("r", "Route for the name generator")
+    .argv
+    ;
 
-var wordList = fs.readFileSync(argv.wordfile).toString().split("\n");
+if (argv.help) {
+    optimist.showHelp();
+    process.exit(code=0);
+}
+
+if (process.env.ENV != "prod") {
+    console.log("Default words / name: " + argv.count);
+    console.log("Using word file: " + argv.file);
+    console.log("Default number of names: " + argv.numnames);
+    console.log("Using port: " + argv.port);
+    console.log("Using route: " + argv.route);
+}
+
+// Load the wordlist
+var wordList = fs.readFileSync(argv.file).toString().split("\n");
 var wordListLength = wordList.length;
-console.log("Read " + wordListLength + " words from file " + argv.wordfile);
+console.log("Read " + wordListLength + " words from file " + argv.file);
 
+// Setup and configure express
 var app = express();
 
-console.log("Using port " + argv.port);
 app.use(express.logger("dev"));
 app.listen(argv.port);
 
-app.get("/namegen", function(request, response) {
+// Routes
+app.get(argv.route, function(request, response) {
     var num = parseInt(request["query"]["num"]) || argv.numnames;
     var words = parseInt(request["query"]["words"]) || argv.count;
     var bandNames = "";
